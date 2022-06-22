f96eaa8b0d11de71e68da58686430729a5c66cae
Lucas Almeida <lucas.almeida@qesh.ai>
2022-06-22 09:20:48 -0300

    :hammer:

---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const Home = () => {
+  return <Text style={{ color: "#ff0000" }}>TESTE</Text>;
+};
+
+export default Home;
